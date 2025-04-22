31f34a784f64d4d99930d8f795eeba4979f9dc4d
uw819kt <miya0arima@gmail.com>
2025-04-22 22:14:06 +0900

    feat: 詳細画面追加

--- app/controllers/drive_af_logs_controller.rb
+++ app/controllers/drive_af_logs_controller.rb
@@ -1,7 +1,22 @@
 class DriveAfLogsController < ApplicationController
   def new
+    @drive_af_log = DriveAfLog.new
   end
 
   def create
+    @drive_af_log = DriveAfLog.new(drive_af_log_params)
+    @drive_af_log.check_time = Time.zone.now
+
+    if @drive_af_log.save
+      redirect_to alcohol_logs_path, notice: "運転前の記録を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def drive_af_log_params
+    params.require(:drive_af_log).permit(:check_time, :confirmation, :detector_used, :result, :condition, :log_remarks, :user_id, :car_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   scope :paid_leaves, only: [:index] do
     resources :requests, only: [:create, :new]
-    resources :approval, only: [:edit, :update, :show]
+    resources :approvals, only: [:new, :create, :edit, :update, :show]
   end
   resources :alcohol_logs, only: [:index, :show, :new, :create, :edit, :update]
   root to: 'paid_leaves#index'
