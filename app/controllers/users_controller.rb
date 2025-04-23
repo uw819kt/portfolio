6c88f7bfa8ad4f6909e7ef12523991a37bd7ac00
uw819kt <miya0arima@gmail.com>
2025-04-23 20:27:36 +0900

    fix: ccan_ruby error修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :department, :admin, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :department)
   end
 end
