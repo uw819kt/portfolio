a30e1b99b953ed148a0f888586208e8e31bd10f5
uw819kt <miya0arima@gmail.com>
2025-05-21 17:15:35 +0900

    test: mailers/ spec追加

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
-class UserMailer < Devise::Mailer
+class UserMailer < Devise::Passwordless::Mailer
   helper :application
   default template_path: "devise/mailer"
 
   def magic_link(record, token, opts = {})
     opts[:subject] = "ログイン用リンクのご案内"
-    super
+    opts[:body] = "ログインリンク: #{token}"
+    super(record, token, opts)
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
+# bundle exec rspec spec/mailers/user_mailer_spec.rb
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "magic_linkメールのテスト" do
+    let(:user) { create(:user) }
+    let(:token) { Devise.friendly_token }
+
+    subject(:mail) { described_class.magic_link(user, token) }
+
+    context "登録したメールアドレスを送信した場合" do
+      it "件名が表示される" do
+      expect(mail.subject).to eq("ログイン用リンクのご案内")
+    end
+
+    it "正しいユーザーに送信する" do
+      expect(mail.to).to eq([ user.email ])
+    end
+
+    it "本文にトークンが含まれる" do
+      expect(mail.body.encoded).to include(token)
+    end
+    end
+  end
 end
